0a1f190bf290611f70e81019a0f75d015f89a89a
Ropke13 <robertas.trumpauskas@ktu.edu>
2021-10-30 14:27:55 +0300

    Administratoriaus Layout
    Sukurtas Administratoriaus Layout.
    Sutvarkytas css.

--- Mokyklu IS/Pages/Registration.cshtml.cs
+++ Mokyklu IS/Pages/Registration.cshtml.cs
@@ -4,11 +4,18 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
 namespace Mokyklu_IS.Pages
 {
     public class RegistrationModel : PageModel
     {
+        private readonly ILogger<RegistrationModel> _logger;
+
+        public RegistrationModel(ILogger<RegistrationModel> logger)
+        {
+            _logger = logger;
+        }
         public void OnGet()
         {
         }
