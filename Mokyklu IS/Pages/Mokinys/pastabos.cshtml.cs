6213433952ad94eed1b519ac297a58cfc91c830f
ADaunys <augustas.daunys@ktu.edu>
2021-10-31 16:20:03 +0200

    2021-10-31 Mokiniai
    Mokiniu posisteme

---
+++ Mokyklu IS/Pages/Mokinys/pastabos.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Mokyklu_IS.Pages.Mokinys
+{
+    public class pastabosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
