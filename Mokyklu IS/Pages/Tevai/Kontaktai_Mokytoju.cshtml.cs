4625ad5849437171bbf460e49c1911cc7cbaa04c
Ropke13 <robertas.trumpauskas@ktu.edu>
2021-11-02 15:21:40 +0200

    tevu
    tevu posisteme

---
+++ Mokyklu IS/Pages/Tevai/Kontaktai_Mokytoju.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Mokyklu_IS.Pages.Tevai
+{
+    public class Kontaktai_MokytojuModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
