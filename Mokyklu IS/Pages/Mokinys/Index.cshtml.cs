dba9047511ca8aea322ca375ea943acea2371899
ADaunys <augustas.daunys@ktu.edu>
2021-12-11 17:45:43 +0200

    Back-End mokiniai
    Beveik baigta mokiniu posisteme

--- Mokyklu IS/Pages/Mokinys/Index.cshtml.cs
+++ Mokyklu IS/Pages/Mokinys/Index.cshtml.cs
@@ -11,19 +11,9 @@ namespace Mokyklu_IS.Pages.Mokinys
 {
     public class IndexModel : PageModel
     {
-        private readonly ApplicationDbContext _db;
-
-        public IndexModel(ApplicationDbContext db)
-        {
-            _db = db;
-        }
-        [BindProperty]
-        public Registracija Registracija { get; set; }
-        public IEnumerable<Model.Mokinys> Mokinys { get; set; }
         public async Task OnGet(string ID)
         {
-            Mokinys = await _db.Mokinys.ToListAsync();
-            Registracija = await _db.Registracija.FindAsync(int.Parse(ID));
+
         }
     }
 }
